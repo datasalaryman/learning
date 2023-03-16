0dd7104455e1e20a7a4f977b591ab50366a01349
Data Salaryman <datasalaryman@protonmail.com>
2023-03-16 11:18:00 +0800

    fix(learn-astro): use slugify function

---
+++ learn-astro/src/utils.ts
@@ -0,0 +1,23 @@
+
+// slugify function from - https://flaviocopes.com/how-to-slugify-a-string-in-javascript/
+export function slugify(str:string) : string {
+  // Remove leading and trailing whitespace
+  str = str.trim()
+
+  // Make the string lowercase
+  str = str.toLowerCase()
+
+  // Remove accents, swap ñ for n, etc
+  str = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+  // Remove invalid characters
+  str = str.replace(/[^a-z0-9 -]/g, '')
+
+  // Replace whitespace with a hyphen
+  str = str.replace(/\s+/g, '-')
+
+  // Collapse consecutive hyphens
+  str = str.replace(/-+/g, '-')
+
+  return str
+}
