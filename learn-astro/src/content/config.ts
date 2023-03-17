1c6eff367e17147e5ce3abf53fe8994a81559ac1
Data Salaryman <datasalaryman@protonmail.com>
2023-03-17 15:58:38 +0800

    feat(learn-astro): add base layout

--- learn-astro/src/content/config.ts
+++ learn-astro/src/content/config.ts
@@ -4,6 +4,8 @@ import { z, defineCollection } from 'astro:content';
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    author: z.string(),
+    date: z.string()
   }),
 });
 // 3. Export a single `collections` object to register your collection(s)
