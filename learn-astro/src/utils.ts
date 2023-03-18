a968ccb6f5a4f0635a7f1de7b7998db9bef62634
Data Salaryman <datasalaryman@protonmail.com>
2023-03-18 16:34:53 +0800

    fix(learn-astro): add content styles to layouts

--- learn-astro/src/utils.ts
+++ learn-astro/src/utils.ts
@@ -1,23 +1,22 @@
-
 // slugify function from - https://flaviocopes.com/how-to-slugify-a-string-in-javascript/
-export function slugify(str:string) : string {
+export function slugify(str: string): string {
   // Remove leading and trailing whitespace
-  str = str.trim()
+  str = str.trim();
 
   // Make the string lowercase
-  str = str.toLowerCase()
+  str = str.toLowerCase();
 
   // Remove accents, swap ñ for n, etc
-  str = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
   // Remove invalid characters
-  str = str.replace(/[^a-z0-9 -]/g, '')
+  str = str.replace(/[^a-z0-9 -]/g, "");
 
   // Replace whitespace with a hyphen
-  str = str.replace(/\s+/g, '-')
+  str = str.replace(/\s+/g, "-");
 
   // Collapse consecutive hyphens
-  str = str.replace(/-+/g, '-')
+  str = str.replace(/-+/g, "-");
 
-  return str
+  return str;
 }
