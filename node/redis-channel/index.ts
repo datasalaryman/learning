60057ae9423d61dd99410c10477dc6dead52bba5
Data Salaryman <datasalaryman@protonmail.com>
2024-06-19 10:49:50 +0800

    feat(node/redis-channel): start project on redis channels

---
+++ node/redis-channel/index.ts
@@ -0,0 +1,18 @@
+import { Redis } from "@upstash/redis";
+import { env } from "./env"
+
+console.log("Hello redis-channel, via Bun!");
+
+const redis = new Redis({
+  url: env.UPSTASH_URL,
+  token: env.UPSTASH_TOKEN,
+});
+
+// string
+// await redis.set('key', '45', {
+//   ex: 60
+// });
+
+let data = await redis.get('key');
+
+console.log(data)
