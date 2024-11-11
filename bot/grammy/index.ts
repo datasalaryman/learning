524158c4592ccbb01c462cb6ce9bb6603f2d130c
Data Salaryman <datasalaryman@protonmail.com>
2024-11-11 14:43:26 +0800

    fix(bot/grammy): add minimal grammy example

--- bot/grammy/index.ts
+++ bot/grammy/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { Bot } from "grammy";
+import { env } from "./env";
+
+// Create a bot object
+const bot = new Bot(env.TG_BOT_KEY); // <-- place your bot token in this string
+
+// Register listeners to handle messages
+bot.on(
+  "message:text", 
+  (ctx) => {
+    console.log(`Received message from user: ${ctx.from.id} - \"${ctx.message.text}\"`)
+    ctx.reply("Echo: " + ctx.message.text)
+  });
+
+// Start the bot (using long polling)
+bot.start();
