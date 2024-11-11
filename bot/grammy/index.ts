288ba8f91d876bc20bd29dcceb096ffd2c23efd5
Data Salaryman <datasalaryman@protonmail.com>
2024-11-11 14:54:35 +0800

    fix(bot/grammy): add minimal example for command

--- bot/grammy/index.ts
+++ bot/grammy/index.ts
@@ -5,12 +5,20 @@ import { env } from "./env";
 const bot = new Bot(env.TG_BOT_KEY); // <-- place your bot token in this string
 
 // Register listeners to handle messages
-bot.on(
-  "message:text", 
-  (ctx) => {
+// bot.on(
+//   "message:text", 
+//   (ctx) => {
+//     console.log(`Received message from user: ${ctx.from.id} - \"${ctx.message.text}\"`)
+//     ctx.reply("Echo: " + ctx.message.text)
+//   });
+
+bot.command(
+  "welcome", 
+  async (ctx) => {
     console.log(`Received message from user: ${ctx.from.id} - \"${ctx.message.text}\"`)
-    ctx.reply("Echo: " + ctx.message.text)
-  });
+    await bot.api.sendMessage(ctx.from.id, "Hello, and welcome to your personal bot!");
+  }
+);
 
 // Start the bot (using long polling)
 bot.start();
